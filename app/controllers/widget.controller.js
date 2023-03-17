863b413a26c5b68c5199490e5f94effaa0f77fad
mohd mustakim <mohd.mustakim@volansinfo.com>
2023-03-17 10:41:31 +0530

    added widget validation and getwidget by id

--- app/controllers/widget.controller.js
+++ app/controllers/widget.controller.js
@@ -1,5 +1,6 @@
 const db = require("../models");
 const config = require("../config/auth.config");
+const pagination = require("../middleware/pagination")
 const Widget = db.WidgetPage;
 const Op = db.Sequelize.Op;
 
@@ -8,12 +9,22 @@ const bcrypt = require("bcryptjs");
 
 exports.widgetAdd = async (req, res) => {
     try {
+        if ((req.body.title).trim() == "") {
+            return res.status(400).send({ message: "Please enter title!" })
+        }
+        if ((req.body.status).trim() == "") {
+            return res.status(400).send({ message: "Status value should not empty!" })
+        }
+        else if (!(req.body.status == 0) && !(req.body.status == 1)) {
+            return res.status(400).send({ message: "Status value should be 0 & 1 only!" })
+        }
         const widget = await Widget.create({
             title: req.body.title,
             description: req.body.description,
             status: req.body.status
         });
-        res.status(200).send({ success: true, message: "widget add successfully:" });
+
+        res.status(200).send({ success: true, message: "Widget added successfully!" });
     } catch (error) {
         return res.status(500).send({ success: false, message: error.message });
     }
@@ -21,9 +32,36 @@ exports.widgetAdd = async (req, res) => {
 
 exports.getAllWidgets = async (req, res) => {
     try {
-        let data = await Widget.findAll();
+        const { limit, offset } = pagination.getPagination(req.query.page, 10)
+
+        const allWidget = await Widget.findAndCountAll({
+            limit,
+            offset,
+            order: [
+                ['id', 'DESC']
+            ]
+        })
+        const response = pagination.getPaginationData(allWidget, req.query.page, limit)
+        res.status(200).json({ success: true, data: response });
+
+    } catch (error) {
+        return res.status(500).send({ success: false, message: error.message });
+    }
+
+};
+exports.getWidget = async (req, res) => {
+    try {
+        const widgetId = req.params.id;
+        const widgetData = await Widget.findOne({
+            where: {
+                id: widgetId,
+            },
+        });
+        if (!widgetData) {
+            return res.status(404).send({ success: false, message: "Widget Not found!" });
+        }
         let response = {
-            widgetData: data
+            widgetData: widgetData
         }
         res.status(200).json(response);
 
@@ -43,7 +81,16 @@ exports.updateWidget = async (req, res) => {
             },
         });
         if (!widgetData) {
-            return res.status(404).send({ success:false, message: "Widget Not found!" });
+            return res.status(404).send({ success: false, message: "Widget Not found!" });
+        }
+        if ((req.body.title).trim() == "") {
+            return res.status(400).send({ message: "Please enter title!" })
+        }
+        if ((req.body.status).trim() == "") {
+            return res.status(400).send({ message: "Status value should not empty!" })
+        }
+        else if (!(req.body.status == 0) && !(req.body.status == 1)) {
+            return res.status(400).send({ message: "Status value should be 0 & 1 only!" })
         }
         const widget = await Widget.update({
             title: req.body.title,
@@ -52,9 +99,9 @@ exports.updateWidget = async (req, res) => {
         },
             { where: { id: widgetId } }
         );
-        return res.status(200).send({ success:true, message: 'Widget data updated successfully!' });
+        return res.status(200).send({ success: true, message: 'Widget data updated successfully!' });
     } catch (error) {
-        return res.status(500).send({ success:false, message: error.message });
+        return res.status(500).send({ success: false, message: error.message });
     }
 };
 
@@ -62,22 +109,37 @@ exports.widgetstatus = async (req, res) => {
     try {
         const widgetId = req.params.id;
         const widgetStatus = req.body.status;
+        const widget = await Widget.findOne({
+            where: {
+                id: widgetId,
+            },
+        });
+        if (!(widget)) {
+            return res.status(404).send({ message: "Widget Not found!" })
+        }
+        if ((req.body.status).trim() == "") {
+            return res.status(400).send({ message: "Status value should not empty!" })
+        }
+        else if (!(req.body.status == 0) && !(req.body.status == 1)) {
+            return res.status(400).send({ message: "Status value should be 0 & 1 only!" })
+        }
+
         if (widgetStatus == 1) {
             const result = await Widget.update(
                 { status: widgetStatus },
                 { where: { id: widgetId } }
             )
-            res.status(200).send({ success:true, message: "widget has been active!" });
+            res.status(200).send({ success: true, message: "Widget has been active!" });
         } else {
             const result = await Widget.update(
                 { status: widgetStatus },
                 { where: { id: widgetId } }
             )
-            res.status(200).send({ success:true, message: "Widget has bee deactivate!" });
+            res.status(200).send({ success: true, message: "Widget has bee deactivate!" });
         }
 
     } catch (eror) {
-        return res.status(500).send({ success:false, message: error.message });
+        return res.status(500).send({ success: false, message: error.message });
     }
 };
 
@@ -90,7 +152,7 @@ exports.widgetDelete = async (req, res) => {
             },
         });
         if (!widget) {
-            return res.status(404).send({ success:false, message: "Widget Not found!" });
+            return res.status(404).send({ success: false, message: "Widget Not found!" });
         }
         const widgetdelete = await Widget.destroy({
             where: {
@@ -99,8 +161,8 @@ exports.widgetDelete = async (req, res) => {
 
         })
 
-        res.status(200).send({ success:true, message: "Widget was deleted successfully!" });
+        res.status(200).send({ success: true, message: "Widget was deleted successfully!" });
     } catch (error) {
-        return res.status(500).send({ success:false, message: error.message });
+        return res.status(500).send({ success: false, message: error.message });
     }
 }
--- app/models/widget.model.js
+++ app/models/widget.model.js
@@ -1,30 +1,28 @@
-module.exports =(sequelize,Sequelize,DataTypes)=>{
-    const WidgetPage = sequelize.define("hiv_widgets",{
-        id:{
+module.exports = (sequelize, Sequelize, DataTypes) => {
+    const WidgetPage = sequelize.define("hiv_widgets", {
+        id: {
             type: DataTypes.INTEGER,
             autoIncrement: true,
             primaryKey: true,
-            require:false
+            require: false
         },
-        title:{
-            type:Sequelize.TEXT,
-            allowNull: true,
-            validate: {
-                notEmpty:false,
-            }
-        },
-        description:{
+        title: {
             type: Sequelize.TEXT,
             allowNull: true,
             validate: {
                 notEmpty: false,
-              }
+            }
+        },
+        description: {
+            type: Sequelize.TEXT,
+            allowNull: true,
+
         },
         status: {
-            type:   Sequelize.ENUM('0', '1'),
+            type: Sequelize.ENUM('0', '1'),
             defaultValue: '0',
             comment: "0-pending,1-active"
-          }
+        }
     });
     return WidgetPage;
 };

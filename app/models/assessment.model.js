ee5a64cde00215c87d4d0b84d5d150181ae6cdc6
Vipul Choudhary <vipul.choudhary@volansinfo.com>
2023-03-20 17:33:33 +0530

    Add some changes into assessment model

--- app/models/assessment.model.js
+++ app/models/assessment.model.js
@@ -12,44 +12,45 @@ module.exports = (sequelize, Sequelize, DataTypes) => {
         },
         assessmentResponseType: {
             type: Sequelize.ENUM('1', '2', '3', '4'),
-            defaultValue: '1',
             comment: "1-video,2-audio,3-text,4-MCQ",
             allowNull: false,
         },
         assessmentPurpose: {
             type: Sequelize.ENUM('1', '2', '3', '4', '5'),
-            defaultValue: '1',
             comment: "1-speaking,2-listening,3-reading,4-writing,5-others",
             allowNull: false,
         },
         assessmentAILevel: {
             type: Sequelize.ENUM('1', '2', '3'),
-            defaultValue: '1',
             comment: "1-basic,2-intermediate,3-advanced",
             allowNull: true,
         },
-        studentId:{
+        assessmentStatusType: {
+            type: Sequelize.ENUM('1', '2', '3', '4'),
+            comment: "1-self,2-assigned,3-completed,4-all report"
+        },
+        studentId: {
             type: Sequelize.INTEGER,
             allowNull: false,
         },
-        teacherId:{
+        teacherId: {
             type: Sequelize.INTEGER,
             allowNull: false,
         },
-        status:{
-            type: Sequelize.ENUM('0','1'),
-            defaultValue:'0',
+        status: {
+            type: Sequelize.ENUM('0', '1'),
+            defaultValue: '0',
             comment: "0-deactive,1-active"
         }
-    },{
-        indexes:[
+    }, {
+        indexes: [
             {
                 unique: false,
-                fields:['studentId']
+                fields: ['studentId']
             },
             {
                 unique: false,
-                fields:['teacherId']
+                fields: ['teacherId']
             }
         ]
     })

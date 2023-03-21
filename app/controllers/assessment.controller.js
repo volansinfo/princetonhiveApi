c95442bd64675027a7cb9c1d2dbd4f70ac7bbdf1
pankaj201k <pankaj.kumar@volansinfo.com>
2023-03-21 18:58:06 +0530

    resolving issues

--- app/controllers/assessment.controller.js
+++ app/controllers/assessment.controller.js
@@ -1,9 +1,10 @@
 const db = require("../models");
 const Assessment = db.assignment;
 const pagination = require("../middleware/pagination");
+const User = db.user;
 exports.assignment = async (req, res) => {
   try {
-    data = req.body;
+    const data = req.body;
     const {
       assessmentName,
       assessmentResponseType,
@@ -23,6 +24,49 @@ exports.assignment = async (req, res) => {
       return res.status(400).send({
         status: false,
         message: "Please Enter Assessment Response Type",
+      });
+    } else if (
+      assessmentResponseType != "1" &&
+      assessmentResponseType != "2" &&
+      assessmentResponseType != "3" &&
+      assessmentResponseType != "4"
+    ) {
+      return res.status(400).send({
+        status: false,
+        message:
+          "Please Enter valid enum like [1,2,3,4] of assessment response type",
+      });
+    } else if (
+      assessmentPurpose != "1" &&
+      assessmentPurpose != "2" &&
+      assessmentPurpose != "3" &&
+      assessmentPurpose != "4" &&
+      assessmentPurpose != "5"
+    ) {
+      return res.status(400).send({
+        status: false,
+        message:
+          "Please Enter valid enum like [1,2,3,4,5] of assessment purpose",
+      });
+    } else if (
+      assessmentAILevel != "1" &&
+      assessmentAILevel != "2" &&
+      assessmentAILevel != "3"
+    ) {
+      return res.status(400).send({
+        status: false,
+        message: "Please Enter valid enum like [1,2,3] of assessment AI level",
+      });
+    } else if (
+      assessmentStatusType != "1" &&
+      assessmentStatusType != "2" &&
+      assessmentStatusType != "3" &&
+      assessmentStatusType != "4"
+    ) {
+      return res.status(400).send({
+        status: false,
+        message:
+          "Please enter valid enum like [1,2,3,4] of assessment status type",
       });
     } else if (!assessmentPurpose) {
       return res
@@ -44,11 +88,16 @@ exports.assignment = async (req, res) => {
     } else if (!teacherId) {
       return res
         .status(400)
-        .send({ status: false, message: "Please Enter teacherId" });
+        .send({ status: false, message: "Please Enter StudentId" });
     } else if (!status) {
       return res
         .status(400)
         .send({ status: false, message: "Please Enter Status" });
+    } else if (status != "0" && status != "1") {
+      return res.status(400).send({
+        status: false,
+        message: "Please enter valid enum like [0,1] of status type",
+      });
     }
 
     const findStudentId = await Assessment.findAll();
@@ -59,7 +108,7 @@ exports.assignment = async (req, res) => {
       ) {
         return res.status(400).send({
           status: false,
-          message: "Assessment Name is  Already Exist",
+          message: "Assessment name is  already exist",
         });
       }
       // console.log(
@@ -68,14 +117,33 @@ exports.assignment = async (req, res) => {
       //   "findassessement"
       // );
     }
-    // console.log(findAssessement[0].studentId, "findassessement");
 
-    const assessment = await Assessment.create(data);
-    return res.status(201).send({
-      status: 201,
-      message: "created Assignment successfully",
-      data: assessment,
-    });
+    const isStundentAndTeacherExist = await User.findAll();
+    for (let i = 0; i < isStundentAndTeacherExist.length; i++) {
+      if (
+        isStundentAndTeacherExist[i].id == studentId &&
+        isStundentAndTeacherExist[i].id == teacherId
+      ) {
+        const assessment = await Assessment.create(data);
+        return res.status(201).send({
+          status: 201,
+          message: "Assessment created successfully ",
+          data: assessment,
+        });
+      } else {
+        return res.status(400).send({
+          status: false,
+          message: "Student or Teacher does not exist",
+        });
+      }
+    }
+
+    // const assessment = await Assessment.create(data);
+    // return res.status(201).send({
+    //   status: 201,
+    //   message: "created Assignment successfully",
+    //   data: assessment,
+    // });
   } catch (error) {
     return res.status(500).send(error);
   }
@@ -84,13 +152,10 @@ exports.assignment = async (req, res) => {
 exports.getAllAssessment = async (req, res) => {
   try {
     const { limit, offset } = pagination.getPagination(req.query.page, 10);
-    const studentId = req.params.studentId;
+
     const results = await Assessment.findAndCountAll({
       limit,
       offset,
-      where: {
-        studentId: studentId,
-      },
       order: [["id", "DESC"]],
     });
 
@@ -99,9 +164,15 @@ exports.getAllAssessment = async (req, res) => {
       req.query.page,
       limit
     );
-    res.status(200).send({
+    if (response.dataItems.length <= 0) {
+      return res
+        .status(404)
+        .send({ status: false, message: "Assessment not available" });
+    }
+    // console.log(response.dataItems, "aaaaaa");
+    return res.status(200).send({
       status: true,
-      message: "data find successfully",
+      message: "Data found successfully ",
       data: response,
     });
   } catch (error) {
@@ -112,12 +183,11 @@ exports.getAllAssessment = async (req, res) => {
 exports.getAssessmentBySelf = async (req, res) => {
   try {
     const { limit, offset } = pagination.getPagination(req.query.page, 10);
-    const studentId = req.params.studentId;
+    // const studentId = req.params.studentId;
     const results = await Assessment.findAndCountAll({
       limit,
       offset,
       where: {
-        studentId: studentId,
         assessmentStatusType: "1",
       },
       order: [["id", "DESC"]],
@@ -128,9 +198,14 @@ exports.getAssessmentBySelf = async (req, res) => {
       req.query.page,
       limit
     );
+    if (response.dataItems.length <= 0) {
+      return res
+        .status(404)
+        .send({ status: false, message: "Assessment not available" });
+    }
     res.status(200).send({
       status: true,
-      message: "data find successfully",
+      message: "Data found successfully ",
       data: response,
     });
   } catch (error) {
@@ -141,12 +216,11 @@ exports.getAssessmentBySelf = async (req, res) => {
 exports.getAssessmentByAssigned = async (req, res) => {
   try {
     const { limit, offset } = pagination.getPagination(req.query.page, 10);
-    const studentId = req.params.studentId;
+    // const studentId = req.params.studentId;
     const results = await Assessment.findAndCountAll({
       limit,
       offset,
       where: {
-        studentId: studentId,
         assessmentStatusType: "2",
       },
       order: [["id", "DESC"]],
@@ -157,9 +231,14 @@ exports.getAssessmentByAssigned = async (req, res) => {
       req.query.page,
       limit
     );
+    if (response.dataItems.length <= 0) {
+      return res
+        .status(404)
+        .send({ status: false, message: "Assessment not available" });
+    }
     res.status(200).send({
       status: true,
-      message: "data find successfully Only Assigned",
+      message: "Data found successfully ",
       data: response,
     });
   } catch (error) {
@@ -170,12 +249,11 @@ exports.getAssessmentByAssigned = async (req, res) => {
 exports.getAssessmentByCompleted = async (req, res) => {
   try {
     const { limit, offset } = pagination.getPagination(req.query.page, 10);
-    const studentId = req.params.studentId;
+    // const studentId = req.params.studentId;
     const results = await Assessment.findAndCountAll({
       limit,
       offset,
       where: {
-        studentId: studentId,
         assessmentStatusType: "3",
       },
       order: [["id", "DESC"]],
@@ -186,9 +264,14 @@ exports.getAssessmentByCompleted = async (req, res) => {
       req.query.page,
       limit
     );
+    if (response.dataItems.length <= 0) {
+      return res
+        .status(404)
+        .send({ status: false, message: "Assessment not available" });
+    }
     res.status(200).send({
       status: true,
-      message: "data find successfully Only Completed",
+      message: "Data found successfully ",
       data: response,
     });
   } catch (error) {

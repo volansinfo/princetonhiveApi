88d82ac28f30e12e7df326bfa104a8dec99744a6
mukul <mukul.grover@volansinfo.com>
2023-03-29 16:57:13 +0530

    code update to main branch

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   HOST: "localhost",
   USER: "postgres",
-  PASSWORD: "admin@123",
-  DB: "princetonhive_api_local",
+  PASSWORD: "Pri@217#$%36",
+  DB: "princetonhive_api",
   dialect: "postgres",
   pool: {
     max: 5,

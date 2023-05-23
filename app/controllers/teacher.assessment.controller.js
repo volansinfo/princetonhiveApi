2a6db7005fd9d735307df1c83a37a5d582be288c
pankaj201k <you@example.compankaj.kumar@volansinfo.com>
2023-05-23 10:16:50 +0530

    update code

--- app/controllers/teacher.assessment.controller.js
+++ app/controllers/teacher.assessment.controller.js
@@ -1483,17 +1483,17 @@ exports.getStudentDetailsAssinedAssessment = async (req, res) => {
 // }
 
 // for level
-function levelNumber(level) {
-  if (level == "0") {
-    return "Beginner";
-  } else if (level == "1") {
-    return "Intermediate";
-  } else if (level == "2") {
-    return "Expert";
-  } else {
-    return "No level";
-  }
-}
+// function levelNumber(level) {
+//   if (level == "0") {
+//     return "Beginner";
+//   } else if (level == "1") {
+//     return "Intermediate";
+//   } else if (level == "2") {
+//     return "Expert";
+//   } else {
+//     return "No level";
+//   }
+// }
 
 // find question details
 async function qustionDetails(questionIds, fullUrl) {
@@ -1517,7 +1517,7 @@ async function qustionDetails(questionIds, fullUrl) {
         questionName: details.questionName,
         departments: findDepartment.departmentName,
         questionImage: fullUrl + details.questionImgUrl,
-        level: levelNumber(details.level),
+        level: details.level,
       });
     }
     return allQuestions;

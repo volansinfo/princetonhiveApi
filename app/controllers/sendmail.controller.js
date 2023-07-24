6a69f42434df22486c7fe87e817df69f7f68d053
volansinfo <124237447+volansinfo@users.noreply.github.com>
2023-07-24 10:18:55 +0530

    code update to main branch

--- app/controllers/sendmail.controller.js
+++ app/controllers/sendmail.controller.js
@@ -39,35 +39,35 @@ const sendMail = async (
   let info;
   if (type == "forgotPassword") {
     info = await transporter.sendMail({
-      from: `"Hive Step  "<${smtpServer.authUser}>`,
+      from: `"Job Ready Now  "<${smtpServer.authUser}>`,
       to: userEmail,
-      subject: "Hive Step new password",
-      text: "Hello from the Hive Step side.",
+      subject: "Job Ready Now new password",
+      text: "Hello from the Job Ready Now side.",
       html: `Your password has been changed and your new password is:<b><h1> ${generatedPwd}</h1></b>`,
     });
   } else if (type == "contactUs") {
     info = await transporter.sendMail({
-      from: `"Hive Step  "<${smtpServer.authUser}>`,
+      from: `"Job Ready Now  "<${smtpServer.authUser}>`,
       to: "parvez.aalam@volansinfo.com",
-      subject: "Welcome to MyLanguage.AI",
-      text: "Welcome to MyLanguage.AI",
-      html: `Welcome to MyLanguage.AI<br><br>
+      subject: "Welcome to Job Ready Now",
+      text: "Welcome to Job Ready Now",
+      html: `Welcome to Job Ready Now<br><br>
                     Name: ${username}<br><br>
                     Email: ${userEmail}<br><br>
                      Phone Number: ${phoneNumber}<br><br>
                      Message: ${message ? message : "No message"}
                     <br><br>
                     Thanks<br>
-                    MyLanguage.AI Team<br>
+                    Job Ready Now Team<br>
                     <br>`,
     });
   } else {
     info = await transporter.sendMail({
       from: `"Hive Step  "<${smtpServer.authUser}>`,
       to: userEmail,
-      subject: "Welcome to Hive Step",
-      text: "Hello from the Hive Step side.",
-      html: `Welcome to hive steps<br><br>
+      subject: "Welcome to Job Ready Now",
+      text: "Hello from the Job Ready Now side.",
+      html: `Welcome to Job Ready Now<br><br>
             Dear  ${username},<br><br>
             Congratulations! Your sign-up process is completed with mylearning.. We are thrilled to welcome you to our team. Thank you for signing up and joining our vibrant community of dedicated educators who are passionate about transforming the world of education.<br><br>
             Since you have recently joined, share your user ID and password.<br><br>
@@ -76,7 +76,7 @@ const sendMail = async (
             Email : <b>${userEmail}</b><br>Password : <b> ${generatedPwd}</b>
             <br><br>
             Thanks<br>
-            Hive Steps Team<br>
+            Job Ready Now Team<br>
             <br>`,
     });
   }
